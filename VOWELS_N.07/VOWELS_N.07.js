6f315570d79c7c3d1eaadf061ed26b4e5455aeea
SashaDeviaten <sasha_deviaten@mail.ru>
2018-03-13 09:28:49 +0300

    add N.07

---
+++ VOWELS_N.07/VOWELS_N.07.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let str = prompt('Введите строку');
+alert(getRussianVowels(str));
+
+function getRussianVowels(str) {
+    str=str.toLowerCase().split('');
+    let russianVowels = 'аоэиуыеёюя';
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (russianVowels.indexOf(str[i]) !== -1)
+            ++count;
+    }
+    return count;
+}
